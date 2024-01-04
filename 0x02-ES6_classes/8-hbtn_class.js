12f476f8326bba71d76a03eb1ae29e67b3a68e25
Ahmed Walid <ahmedwalid@pop-os.localdomain>
2024-01-04 20:24:11 +0200

    ES6_Classes

---
+++ 0x02-ES6_classes/8-hbtn_class.js
@@ -0,0 +1,28 @@
+export default class HolbertonClass {
+  constructor(size, location) {
+    this.size = size;
+    this.location = location;
+  }
+
+  get size() {
+    return this._size;
+  }
+
+  set size(value) {
+    this._size = value;
+  }
+
+  get location() {
+    return this._location;
+  }
+
+  set location(value) {
+    this._location = value;
+  }
+
+  [Symbol.toPrimitive](prop) {
+    if (typeof prop === 'number') return this.size;
+    if (typeof prop === 'string') return this.location;
+    return this;
+  }
+}
