e16a75a92362697e8943b4d43db47df552701287
Ahmed Walid <ahmedwalid@pop-os.localdomain>
2024-01-08 13:30:18 +0200

    ES6_Data_Manipulation

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,3 +1,6 @@
 export default function getStudentsByLocation(students, location) {
-  return students.filter((student) => student.location === location);
+  if (students instanceof Array) {
+    return students.filter((student) => student.location === location);
+  }
+  return [];
 }
