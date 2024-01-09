c8bcfc40aefb949ec74357364828b66b5c5a94e7
Ahmed Walid <ahmedwalid@pop-os.localdomain>
2024-01-09 13:37:13 +0200

    ES6_Data_Manipulation

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,5 +1,8 @@
 export default function createInt8TypedArray(length, position, value) {
+  if (position >= length) {
+    throw new Error('Position outside range');
+  }
   const typeArr = new Int8Array(length);
-  typeArr[position] = value;
-  return typeArr;
+  typeArr.fill(value, position, position + 1);
+  return new DataView(typeArr.buffer);
 }
