b9ef497e3ec60d8972bda38f97eb2261a3bf2ad6
Ahmed Walid <ahmedwalid@pop-os.localdomain>
2024-01-09 13:31:21 +0200

    ES6_Data_Manipulation

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,13 +1,16 @@
 export default function updateStudentGradeByCity(students, city, newGrades) {
-  const defaultGrade = { grade: 'N/A' };
+  if (students instanceof Array) {
+    const defaultGrade = { grade: 'N/A' };
 
-  return students.filter((student) => student.location === city)
-    .map((student) => ({
-      id: student.id,
-      firstName: student.firstName,
-      location: student.location,
-      grades: (newGrades
-        .filter((grade) => grade.studentId === student.id)
-        .pop() || defaultGrade).grade,
-    }));
+    return students.filter((student) => student.location === city)
+      .map((student) => ({
+        id: student.id,
+        firstName: student.firstName,
+        location: student.location,
+        grades: (newGrades
+          .filter((grade) => grade.studentId === student.id)
+          .pop() || defaultGrade).grade,
+      }));
+  }
+  return [];
 }
