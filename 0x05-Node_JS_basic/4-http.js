5f9a91c0b07078e0b5dee05d5053c52288d6ef09
root <root@DESKTOP-4KC1C5P>
2024-02-27 20:12:57 +0200

    http server

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -3,8 +3,8 @@ const http = require('http');
 const app = http.createServer();
 
 app.on('request', (req, res) => {
-  res.end('Hello Holberton School!')
-})
+  res.end('Hello Holberton School!');
+});
 
 app.listen(1245);
 
